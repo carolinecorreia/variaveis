c14e66ec639e5e03bb8af63786f6b3c099c539c2
carolinecorreia <104242341+carolinecorreia@users.noreply.github.com>
2022-06-26 02:05:24 -0300

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,8 @@
+//Crie um algoritmo que calcule a velocidade média de um veículo e retorne o resultado no console.
+//Dica do sucesso: A velocidade média é calculada dividindo a distancia percorrida pelo tempo gasto.
+
+let distancia = 888;
+let tempo = 7;
+const velocidadeMedia = distancia/tempo;
+
+console.log (`A distância média percorrida foi de ${velocidadeMedia} km/h.` )
